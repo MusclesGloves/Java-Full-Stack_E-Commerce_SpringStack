e029aacf575724e26cb4766ee986ed17df22b734
Keshav Rao Sathvick <kesathvick@gmail.com>
2026-02-20 17:23:28 +0530

    deleted duplicate

--- SpringBoot (BACK-END)/src/main/java/com/telusko/springecom/payment/OrderPaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.springecom.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderPaymentRepository extends JpaRepository<OrderPayment, Long> {
-    List<OrderPayment> findByUsernameOrderByCreatedAtDesc(String username);
-    OrderPayment findByOrderId(String orderId);
-}
